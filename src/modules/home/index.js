0093f2aeb550e86fb5ae7383eb517636f3939bb5
Luís Nascimento <luisnascimento.comp@gmail.com>
2018-04-20 02:37:18 -0300

    Including sass loader

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+import './home.style.scss';
+
 import routing from './home.routes';
 import HomeController from './home.controller';
 
