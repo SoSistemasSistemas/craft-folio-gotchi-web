18b42dd9d78248028d4ce03c9378d218a237ef1f
Luís Nascimento <luisnascimento.comp@gmail.com>
2018-04-20 03:37:00 -0300

    Login page - first version

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+import './auth.style.scss';
+
 import routing from './auth.routes';
 import AuthController from './auth.controller';
 import authService from '../../services/auth.service';
