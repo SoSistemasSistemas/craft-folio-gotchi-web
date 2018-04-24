deb859c6d8555c8e5883055e71f25fd8f28d705f
Luís Nascimento <luisnascimento.comp@gmail.com>
2018-04-24 07:32:22 -0300

    Configuring widgets - part 1

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -1,3 +1,22 @@
 export default class HomeController {
 
+  constructor() {
+    this.skyTextures = [
+      'https://storage.googleapis.com/sss-cfg-widgets/sky/sky_1.jpg',
+      'https://storage.googleapis.com/sss-cfg-widgets/sky/sky_2.jpg',
+    ];
+    this.groundTextures = [
+      'https://storage.googleapis.com/sss-cfg-widgets/ground/grass.jpg',
+      'https://storage.googleapis.com/sss-cfg-widgets/ground/rock.png',
+      'https://storage.googleapis.com/sss-cfg-widgets/ground/sand.jpg',
+    ];
+  }
+
+  openWidgetConfiguration() {
+    document.getElementById("mySidenav").style.width = "500px";
+  };
+  
+  closeWidgetConfiguration() {
+    document.getElementById("mySidenav").style.width = "0";
+  };
 }
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,6 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './home.style.scss';
 
 import routing from './home.routes';
