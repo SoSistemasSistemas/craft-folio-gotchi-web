8216de370270b4883aac99b5324f0c6637bb1c07
Luís Nascimento <luisnascimento.comp@gmail.com>
2018-05-02 05:08:42 -0300

    Fix lint

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -45,9 +45,13 @@ export default class HomeController {
     const avatarEl = document.getElementById('avatar');
 
     function move(size) {
-      const position = Math.abs(parseInt(avatarEl.style.left || 0, 10)) + (avatarEl.offsetWidth || 0);
-      if (position + Math.abs(size) < window.innerWidth || (parseInt(avatarEl.style.left, 10) / size < 0)) {
-        avatarEl.style.left = avatarEl.style.left ? `${parseInt(avatarEl.style.left, 10) + size}px` : `${size}px`;
+      const position =
+        Math.abs(parseInt(avatarEl.style.left || 0, 10)) + (avatarEl.offsetWidth || 0);
+      if (position + Math.abs(size) < window.innerWidth ||
+          (parseInt(avatarEl.style.left, 10) / size < 0)) {
+        avatarEl.style.left = avatarEl.style.left ?
+          `${parseInt(avatarEl.style.left, 10) + size}px` :
+          `${size}px`;
       }
     }
 
