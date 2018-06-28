b686f4f336fe637bee11f422114eef431d37d6ec
Felipe Freitas <felipesafreitas@gmail.com>
2018-06-28 08:28:47 -0300

    added performs way to serialize and deserialize objects in passport

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const app = express();
 const { API_PORT } = process.env;
 
-
-
 app.use(morgan('combined', { skip: (_, res) => res.statusCode < BAD_REQUEST }));
 app.use(cors()); // Used to cheat 'Same Origem Policy' from browsers
 
@@ -19,6 +17,15 @@ app.use(express.static(path.join(__dirname, '/dist')));
 app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
 
 app.listen(parseInt(API_PORT, 10), () => {
   console.log(`CraftFolioGotchi up and running at 0.0.0.0:${API_PORT}... :)`);
