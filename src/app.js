88ac7d0714ccf0d168fe2a73c799924917b58e82
Felipe Freitas <felipesafreitas@gmail.com>
2018-06-28 08:34:29 -0300

    added config for auth

--- src/app.js
+++ src/app.js
@@ -31,11 +31,14 @@ passport.use(new FacebookStrategy({
   clientSecret:config.facebook_api_secret ,
   callbackURL: config.callback_url
 }, function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function () {
+    process.nextTick(function () {
     return done(null, profile);
   });
 }
 ));
+
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
 
 app.listen(parseInt(API_PORT, 10), () => {
   console.log(`CraftFolioGotchi up and running at 0.0.0.0:${API_PORT}... :)`);
